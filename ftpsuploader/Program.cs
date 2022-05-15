1b64f57c3c61319597a43080636d2221076d2f36
Peter Gill <peter.gill@townsuite.com>
2022-05-15 10:35:28 -0230

    initial code to test ftps uploads

---
+++ ftpsuploader/Program.cs
@@ -0,0 +1,49 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+using ftpuploader;
+
+string src = string.Empty;
+string dest = string.Empty;
+string host = string.Empty;
+int port = 0;
+string username = string.Empty;
+string password = string.Empty;
+
+if (args.Length > 0)
+{
+    for (int i = 0; i < args.Length; i++)
+    {
+        switch (args[i])
+        {
+            case "-src":
+                src = args[i + 1];
+                break;
+            case "-dest":
+                dest = args[i + 1];
+                break;
+            case "-host":
+                host = args[i + 1];
+                break;
+            case "-port":
+                port = int.Parse(args[i + 1]);
+                break;
+            case "-username":
+                username = args[i + 1];
+                break;
+            case "-password":
+                password = args[i + 1];
+                break;
+        }
+    }
+}
+else
+{
+    Console.WriteLine("No arguments");
+    Environment.Exit(1);
+}
+
+using var up = new Uploader(host, port, username, password);
+await up.UploadFileAsync(src, dest);
+
+Console.WriteLine("😊");
---
+++ ftpsuploader/Uploader.cs
@@ -0,0 +1,45 @@
+using FluentFTP;
+using System.IO;
+using FluentFTP.Helpers;
+
+namespace ftpuploader;
+
+public sealed class Uploader : IDisposable
+{
+    FtpClient client;
+
+    public void Dispose()
+    {
+        if (client.IsConnected)
+        {
+            client.Disconnect();
+        }
+
+        client.Dispose();
+    }
+
+    public Uploader(string host, int port, string username, string password)
+    {
+        client = new FtpClient(host, port, username, password);
+        client.RetryAttempts = 100;
+        client.EncryptionMode = FtpEncryptionMode.Explicit;
+    }
+
+    public async Task UploadFileAsync(string srcPath, string dest)
+    {
+        if (!client.IsConnected)
+        {
+            await client.ConnectAsync();
+        }
+
+        var status = client.UploadFile(srcPath, dest, FtpRemoteExists.Overwrite,
+            true, FtpVerify.OnlyChecksum);
+
+        if (!status.IsSuccess())
+        {
+            // try one more time
+            await client.UploadFileAsync(srcPath, dest, FtpRemoteExists.Overwrite,
+                true, FtpVerify.OnlyChecksum);
+        }
+    }
+}
